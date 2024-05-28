2155b1b25ea08c75389352bda1ab26fa28fe0899
mwrowan89 <mwrowan89@gmail.com>
2024-05-28 17:14:17 -0600

    added axios and api key

--- streaming-app/src/App.js
+++ streaming-app/src/App.js
@@ -1,7 +1,13 @@
-
+import {useState} from 'react'
 import './App.css';
+import axios from 'axios'
 
-function App() {
+export default function App() {
+  const [title, setTitle] = useState(null);
+  const [searchResults, setSearchResults] = useState(null);
+  const [titleDetails, setTitleDetails] = useState(null);
+  const [streamingInfo, setStreamingInfo] = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,11 +16,20 @@ function App() {
         </h1>
         <h5>Find where to stream your favorite TV Shows and Movies from 150+ Streaming platforms</h5>
         
-        <form className="form-container">
+        <form className="form-container"
+        onSubmit={e => {
+          e.preventDefault();
+          e.stopPropagation();
+        }}>
 
         <input className="form"
         type="text"
         placeholder=" Enter a Title"
+        onChange={e => {
+          setTitle(e.target.value);
+          setSearchResults(null);
+          setTitleDetails(null);
+        }}
         />
         <button className="button"
         type="submit">
@@ -26,6 +41,4 @@ function App() {
       
     </div>
   );
-}
-
-export default App;
+};
