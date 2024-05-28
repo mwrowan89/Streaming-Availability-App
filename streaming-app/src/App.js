1dda24044d62e973e6f299da5c21bb1a6a3efb8c
mwrowan89 <mwrowan89@gmail.com>
2024-05-28 16:59:47 -0600

    Created home page

--- streaming-app/src/App.js
+++ streaming-app/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="Title">
+          <span className="text-red">Streaming</span> Availabilty
+        </h1>
+        <h5>Find where to stream your favorite TV Shows and Movies from 150+ Streaming platforms</h5>
+        
+        <form className="form">
+        <input
+        type="text"
+        placeholder="Enter a Title"
+        />
+        <button className="button"
+        type="submit">
+          Search
+        </button>
+        
+      </form>
       </header>
+      
     </div>
   );
 }
