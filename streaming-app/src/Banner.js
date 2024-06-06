55fdc1b20f64cb41b8bfe59abb41e3c00c259153
mwrowan89 <mwrowan89@gmail.com>
2024-06-06 14:34:39 -0600

    Api call in banner working

--- streaming-app/src/Banner.js
+++ streaming-app/src/Banner.js
@@ -1,36 +1,70 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-
 function Banner() {
-    const [titleInfo, setTitleInfo] = useState(null);
-    const [loading, setLoading] = useState(false);
+  const [titleInfo, setTitleInfo] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const releaseYear = 2024;
 
+  useEffect(() => {
+    getInfo(releaseYear);
+  }, [releaseYear]);
 
-    const getInfo = async (movieTitle) => {
-        try {
-          setLoading(true);
-          const result = await axios.get("http://www.omdbapi.com/", {
-            params: {
-              t: movieTitle,
-              apikey: "5aa370ab",
-            },
-          });
-          const { data } = result;
-          setTitleInfo((prevInfo) => ({
-            ...prevInfo,
-            [movieTitle]: data,
-          }));
-          setLoading(false);
-        } catch (error) {
-          console.error("Error fetching data from OMDB API", error);
-          setLoading(false);
-        }
-      };
+  const getInfo = async (releaseYear) => {
+    try {
+      setLoading(true);
+      const result = await axios.get("http://www.omdbapi.com/", {
+        params: {
+          t: "*",
+          y: releaseYear,
+          apikey: "5aa370ab",
+        },
+      });
+      const { data } = result;
+      console.log(data);
+      setTitleInfo((prevInfo) => ({
+        ...prevInfo,
+        [releaseYear]: data.Search || [],
+      }));
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching data from OMDB API", error);
+      setLoading(false);
+    }
+  };
 
   return (
     <div>
-      <h1>Banner</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : titleInfo ? (
+        titleInfo[releaseYear].map((result, index) => (
+          <div key={index} className="result-item">
+            <h3>{result.Title}</h3>
+            <img
+              src={
+                result.Poster && result.Poster !== "N/A"
+                  ? result.Poster
+                  : "./NotFound.jpeg"
+              }
+              alt={`${result.Title} poster`}
+            />
+            {titleInfo[releaseYear] && (
+              <div>
+                <p>Actors: {titleInfo[result.Title].Actors}</p>
+                <p>Genre: {titleInfo[result.Title].Genre}</p>
+                <p>Rated: {titleInfo[result.Title].Rated}</p>
+                {/* Display other info as needed */}
+              </div>
+            )}
+            <p>Year: {result.Year}</p>
+            {/* <p>IMBD Rating: {titleInfo[result.Title].imdbRating}</p> */}
+            <p>Type: {result.Type}</p>
+          </div>
+        ))
+      ) : (
+        <p>No results found.</p>
+      )}
     </div>
   );
 }
