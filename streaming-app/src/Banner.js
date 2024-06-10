e6ff2d357af3caed767516f1aa292b0fbc963561
mwrowan89 <mwrowan89@gmail.com>
2024-06-10 17:31:55 -0600

    Added TV series to banner

--- streaming-app/src/Banner.js
+++ streaming-app/src/Banner.js
@@ -5,9 +5,10 @@ import "./Banner.css";
 function Banner() {
   const [loading, setLoading] = useState(false);
   const [movies, setMovies] = useState(null);
+  const [moreInfo, setMoreInfo] = useState(false);
+  const [tvShows, setTvShows] = useState(null);
 
-
-  const tmdbInfo = async () => {
+  const tmdbMovieInfo = async () => {
     const options = {
       method: "GET",
       url: "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_year=2024&sort_by=popularity.desc",
@@ -23,34 +24,62 @@ function Banner() {
       const response = await axios.request(options);
       setMovies(response.data.results); // Assuming response.data.results contains the list of movies
       setLoading(false);
+      console.log(response);
     } catch (error) {
       console.error("Error fetching data from TMDB API", error);
       setLoading(false);
     }
   };
 
+  const tmbdTvInfo = async () => {
+    const options = {
+      method: "GET",
+      url: "https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMTM3Y2NmZTcxMjY3NjYxMzMzNDVhZjZlOWJmY2Y5ZSIsInN1YiI6IjY2NWU0ZDk4N2U3NGNlNTcyMzIzMWM3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Bws_3Y0C7Tah8B0W1oV4kn9soF-vrTTl803_ccppujI",
+      },
+    };
+    try {
+      setLoading(true);
+      const response = await axios.request(options);
+      setTvShows(response.data.results); // Assuming response.data.results contains the list of movies
+      setLoading(false);
+      console.log(response);
+    } catch (error) {
+      console.error("Error fetching data from TMDB API", error);
+      setLoading(false);
+    }
+  };
+  // const getMoreInfo = ( => {
+
+  // })
+
   useEffect(() => {
-    tmdbInfo();
+    tmdbMovieInfo();
+    tmbdTvInfo();
   }, []);
 
-   
   return (
     <div className="banner-results-conatiner">
-    <h1>Popular Titles from 2024</h1>
+      <h1>Popular Titles from 2024</h1>
       <div className="banner-results">
         {loading ? (
           <p>Loading...</p>
         ) : movies ? (
-          [...movies, ...movies].map((movie, index) => (
+          [...movies, ...tvShows].map((result, index) => (
             <div key={index} className="banner-result-item">
               <img
                 src={
-                  movie.poster_path
-                    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                  result.poster_path
+                    ? `https://image.tmdb.org/t/p/w500${result.poster_path}`
                     : "./NotFound.jpeg"
                 }
-                alt={`${movie.title} poster`}
+                alt={`${result.title} poster`}
+                onClick={!moreInfo}
               />
+              <p>{moreInfo ? <p>{result.adult}</p> : " "}</p>
             </div>
           ))
         ) : (
