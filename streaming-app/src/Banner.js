041cbe93a54d411dca1ed5b7d4a9bea55e12dd5d
mwrowan89 <mwrowan89@gmail.com>
2024-06-11 14:14:31 -0600

    Adjusted more info to use exsisting information instead of additional API call

--- streaming-app/src/Banner.js
+++ streaming-app/src/Banner.js
@@ -4,18 +4,15 @@ import "./Banner.css";
 
 function Banner() {
   const [loading, setLoading] = useState(false);
-  const [movies, setMovies] = useState(null);
-  const [moreInfo, setMoreInfo] = useState({});
-  const [tvShows, setTvShows] = useState(null);
+  const [results, setResults] = useState([]);
   const [selectedTitle, setSelectedTitle] = useState(null);
 
   useEffect(() => {
-    tmdbMovieInfo();
-    tmbdTvInfo();
+    tmdbInfo();
   }, []);
 
-  const tmdbMovieInfo = async () => {
-    const options = {
+  const tmdbInfo = async () => {
+    const movieOptions = {
       method: "GET",
       url: "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_year=2024&sort_by=popularity.desc",
       headers: {
@@ -25,20 +22,7 @@ function Banner() {
       },
     };
 
-    try {
-      setLoading(true);
-      const response = await axios.request(options);
-      setMovies(response.data.results);
-      setLoading(false);
-      console.log(response);
-    } catch (error) {
-      console.error("Error fetching data from TMDB API", error);
-      setLoading(false);
-    }
-  };
-
-  const tmbdTvInfo = async () => {
-    const options = {
+    const tvOptions = {
       method: "GET",
       url: "https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc",
       headers: {
@@ -47,40 +31,29 @@ function Banner() {
           "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMTM3Y2NmZTcxMjY3NjYxMzMzNDVhZjZlOWJmY2Y5ZSIsInN1YiI6IjY2NWU0ZDk4N2U3NGNlNTcyMzIzMWM3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Bws_3Y0C7Tah8B0W1oV4kn9soF-vrTTl803_ccppujI",
       },
     };
+
     try {
       setLoading(true);
-      const response = await axios.request(options);
-      setTvShows(response.data.results); 
+      const [movieResponse, tvResponse] = await Promise.all([
+        axios.request(movieOptions),
+        axios.request(tvOptions),
+      ]);
+
+      const combinedResults = [
+        ...movieResponse.data.results,
+        ...tvResponse.data.results,
+      ];
+      setResults(combinedResults);
       setLoading(false);
-      console.log(response);
+      console.log(combinedResults);
     } catch (error) {
       console.error("Error fetching data from TMDB API", error);
       setLoading(false);
     }
   };
 
-  const getMoreInfo = async (e, title) => {
-    e.preventDefault();
-    try {
-      setLoading(true);
-      const result = await axios.get("http://www.omdbapi.com/", {
-        params: {
-          t: title,
-          apikey: "5aa370ab",
-        },
-      });
-      const { data } = result;
-      setMoreInfo((prevInfo) => ({
-        ...prevInfo,
-        [title]: data,
-      }));
-      setSelectedTitle(title);
-      setLoading(false);
-      console.log(data)
-    } catch (error) {
-      console.error("Error fetching data from OMDB API", error);
-      setLoading(false);
-    }
+  const getMoreInfo = (title) => {
+    setSelectedTitle(title === selectedTitle ? null : title);
   };
 
   return (
@@ -89,11 +62,10 @@ function Banner() {
       <div className="banner-results">
         {loading ? (
           <p>Loading...</p>
-        ) : movies && tvShows ? (
-          [...movies, ...tvShows].map((result, index) => {
+        ) : results.length > 0 ? (
+          results.map((result, index) => {
             const title = result.title || result.name;
-            const info = moreInfo[title];
-            const description = info ? (info.Plot || info.overview) : null;
+            const description = result.overview;
 
             return (
             <div key={index} className="banner-result-item">
@@ -106,12 +78,13 @@ function Banner() {
                 alt={`${title} poster`}
                 onClick={(e) => {
                   e.preventDefault();
-                  getMoreInfo(e, title)}}
+                  getMoreInfo(title)}}
               />
               {selectedTitle === title && (
                 <div className="more-info">
                 <p>{description}</p> 
-                <p onClick={ (e) => {
+                <p className="close-button"
+                onClick={ (e) => {
                   e.preventDefault();
                   setSelectedTitle(null)} }>X</p>
                 </div>
