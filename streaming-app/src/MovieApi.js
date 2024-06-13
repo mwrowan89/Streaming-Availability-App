daf44bae165c0e441ec093d8dbe7afdf7f2fa4af
mwrowan89 <mwrowan89@gmail.com>
2024-06-13 16:37:32 -0600

    Updated movieApi

--- streaming-app/src/MovieApi.js
+++ streaming-app/src/MovieApi.js
@@ -5,6 +5,7 @@ import {tmdbMovieInfo, tmdbTvInfo} from './TmdbApi';
 function MovieApi() {
     const [loading, setLoading] = useState(false);
     const [movieResults, setMovieResults] = useState(null);
+    const [tvResults, setTvResults] = useState(null);
 
     useEffect(() => {
         const fetchMovies = async () => {
@@ -20,10 +21,25 @@ function MovieApi() {
     
         fetchMovies();
       }, []);
+
+      useEffect(() => {
+        const fetchTvShows = async () => {
+          setLoading(true);
+          try {
+            const tvShows = await tmdbTvInfo();
+            setTvResults(tvShows);
+            setLoading(false);
+          } catch (error) {
+            console.error("Error fetching movie data", error);
+          }
+        };
+    
+        fetchTvShows();
+      }, []);
     
   return (
     <div>
-        <div className="result-container">
+        <div className="movie-result-container">
         {loading ? (
               <p>Loading...</p>
             ) : movieResults ? (
@@ -44,7 +60,28 @@ function MovieApi() {
             :(
               <p className="no-results">No search results.</p>
             )}
-
+        </div>
+        <div className="tv-result-container">
+        {loading ? (
+              <p>Loading...</p>
+            ) : tvResults ? (
+              tvResults.map((result, index) => (
+                <div key={index} className="result-item">
+                  
+                  <img
+                    src={
+                      result.poster_path && result.Poster !== "N/A"
+                        ? `https://image.tmdb.org/t/p/w500${result.poster_path}`
+                        : "./NotFound.jpeg"
+                    }
+                    alt={`${result.Title} poster`}
+                  />
+                </div>
+              )
+            ))
+            :(
+              <p className="no-results">No search results.</p>
+            )}
         </div>
     </div>
   )
--- streaming-app/src/TmdbApi.js
+++ streaming-app/src/TmdbApi.js
@@ -5,7 +5,7 @@ const movieOptions = {
   url: "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_year=2024&sort_by=popularity.desc",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer YOUR_ACCESS_TOKEN",
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMTM3Y2NmZTcxMjY3NjYxMzMzNDVhZjZlOWJmY2Y5ZSIsInN1YiI6IjY2NWU0ZDk4N2U3NGNlNTcyMzIzMWM3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Bws_3Y0C7Tah8B0W1oV4kn9soF-vrTTl803_ccppujI",
   },
 };
 
@@ -14,7 +14,7 @@ const tvOptions = {
   url: "https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer YOUR_ACCESS_TOKEN",
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMTM3Y2NmZTcxMjY3NjYxMzMzNDVhZjZlOWJmY2Y5ZSIsInN1YiI6IjY2NWU0ZDk4N2U3NGNlNTcyMzIzMWM3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Bws_3Y0C7Tah8B0W1oV4kn9soF-vrTTl803_ccppujI",
   },
 };
 
