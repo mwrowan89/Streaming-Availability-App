422c09723709a24b9703881f1d8052c5f9943674
mwrowan89 <mwrowan89@gmail.com>
2024-06-18 14:09:43 -0600

    Created header comp

---
+++ streaming-app/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="Title">
+        <span className="text-red">Discover</span> Titles
+      </h1>
+      <h5 className="page-description">
+        Find where to stream your favorite TV Shows and Movies from 150+
+        Streaming platforms
+      </h5>
+    </div>
+  );
+};
+
+export default Header;
