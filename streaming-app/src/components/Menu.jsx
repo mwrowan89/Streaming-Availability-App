5e29b3d2a7a6ea8d09917fb3ea8795612f2bfbaf
mwrowan89 <mwrowan89@gmail.com>
2024-06-20 13:27:12 -0600

    Menu position fixed

--- streaming-app/src/components/Menu.jsx
+++ streaming-app/src/components/Menu.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Menu.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Menu = () => {
+  const SearchIcon = () => {
+    return <FontAwesomeIcon icon={faSearch} />;
+  };
+
   return (
     <div className="menu-main">
-      <h1 className="Title">
+      <h1 className="menu-title">
         <span className="text-red">Discover</span> Titles
       </h1>
 
@@ -12,6 +18,7 @@ const Menu = () => {
         <h3>Movies</h3>
         <h3>TV Shows</h3>
         <h3>People</h3>
+        <SearchIcon />
       </div>
     </div>
   );
