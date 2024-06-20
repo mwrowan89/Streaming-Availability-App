5abcf644948c07a47dbf5f74ca6ae4fb892b9740
mwrowan89 <mwrowan89@gmail.com>
2024-06-20 12:45:08 -0600

    Added menu options

--- streaming-app/src/components/Menu.jsx
+++ streaming-app/src/components/Menu.jsx
@@ -4,9 +4,17 @@ import "./Menu.css";
 const Menu = () => {
   return (
     <div className="menu-main">
-      <div className="hamburger-icon"></div>
-      <div className="hamburger-icon"></div>
-      <div className="hamburger-icon"></div>
+      <div className="hamburger-main">
+        <div className="hamburger-icon"></div>
+        <div className="hamburger-icon"></div>
+        <div className="hamburger-icon"></div>
+      </div>
+
+      <div className="menu-filter-options">
+        <h3>Movies</h3>
+        <h3>TV Shows</h3>
+        <h3>People</h3>
+      </div>
     </div>
   );
 };
