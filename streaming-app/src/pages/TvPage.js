c740d0a96b17ecc4c90da315ac450b9869e22331
mwrowan89 <mwrowan89@gmail.com>
2024-06-20 14:14:41 -0600

    Added same logic to tvPage

--- streaming-app/src/pages/TvPage.js
+++ streaming-app/src/pages/TvPage.js
@@ -1,12 +1,27 @@
 import React from "react";
 import Menu from "../components/Menu";
+import TvApi from "../components/TvApi";
+import { useState } from "react";
 
 const TvPage = () => {
+  const [selectedOption, setSelectedOption] = useState("");
+  const handleChange = (e) => {
+    setSelectedOption(e.target.value);
+  };
   return (
     <>
       <div className="App">
         <Menu />
-        <h1>Search TV Shows</h1>
+        <h2>
+          <label htmlFor="options">Search Movies By: &nbsp;</label>
+          <select id="options" value={selectedOption} onChange={handleChange}>
+            <option value="">Select...</option>
+            <option value="movies">Movies</option>
+            <option value="tv-shows">TV Shows</option>
+            <option value="people">People</option>
+          </select>
+        </h2>
+        <TvApi />
       </div>
     </>
   );
