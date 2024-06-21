36bcaac5ce97955e6c60608b53c089d5996e0086
mwrowan89 <mwrowan89@gmail.com>
2024-06-21 12:16:21 -0600

    Created additional people API

--- streaming-app/src/pages/PeoplePage.js
+++ streaming-app/src/pages/PeoplePage.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Menu from "../components/Menu";
 
 const PeoplePage = () => {
-  return <div>PeoplePage</div>;
+  return (
+    <div className="App">
+      <Menu />
+      <h1>People Search</h1>
+    </div>
+  );
 };
 
 export default PeoplePage;
