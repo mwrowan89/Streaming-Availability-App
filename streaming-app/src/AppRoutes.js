5d0428412e3eb63d0d687bdc51ab49c25b05c096
mwrowan89 <mwrowan89@gmail.com>
2024-06-21 11:43:56 -0600

    Set up route for ppl page

--- streaming-app/src/AppRoutes.js
+++ streaming-app/src/AppRoutes.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import MoviePage from "./pages/MoviePage";
 import TvPage from "./pages/TvPage";
+import PeoplePage from "./pages/PeoplePage";
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/people" element={<PeoplePage />} />
         <Route path="/movies" element={<MoviePage />} />
         <Route path="/tv-shows" element={<TvPage />} />
         <Route path="/" element={<App />} />
