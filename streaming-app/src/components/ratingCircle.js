edacc810f313586dead5a48b738df8d638a7b460
mwrowan89 <mwrowan89@gmail.com>
2024-06-28 10:37:58 -0600

    rating circle started

--- streaming-app/src/components/ratingCircle.js
+++ streaming-app/src/components/ratingCircle.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const ratingCircle = () => {
-  return <div>ratingCircle</div>;
+  return (
+    <div className="progress">
+      <button>Start</button>
+      <button>Reset</button>
+    </div>
+  );
 };
 
 export default ratingCircle;
