1fc28b0d9d44a0b4e17266edf32e5194922b8f81
mwrowan89 <mwrowan89@gmail.com>
2024-06-28 15:30:39 -0600

    Colors displaying but showing like progress bar

--- streaming-app/src/components/ratingCircle.js
+++ streaming-app/src/components/ratingCircle.js
@@ -3,10 +3,26 @@ import "./RatingCircle.css";
 
 const RatingCircle = ({ value }) => {
   const roundedValue = Math.round(value);
+
+  let colorClass = "";
+
+  if (roundedValue >= 0 && roundedValue < 25) {
+    colorClass = "red";
+  } else if (roundedValue >= 25 && roundedValue < 50) {
+    colorClass = "orange";
+  } else if (roundedValue >= 50 && roundedValue < 75) {
+    colorClass = "yellow";
+  } else if (roundedValue >= 75 && roundedValue <= 100) {
+    colorClass = "green";
+  }
+
   return (
     <div>
       {value && (
-        <div className="progress" data-value={roundedValue + "%"}></div>
+        <div
+          className={`progress ${colorClass}`}
+          data-value={roundedValue + "%"}
+        ></div>
       )}
     </div>
   );
