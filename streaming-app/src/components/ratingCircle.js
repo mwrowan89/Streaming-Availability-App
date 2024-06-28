16ddbddc48bfd4c6c49741f6e94eaa5eb1b04333
mwrowan89 <mwrowan89@gmail.com>
2024-06-28 15:23:55 -0600

    Rating circle displaying properly with percentages

--- streaming-app/src/components/ratingCircle.js
+++ streaming-app/src/components/ratingCircle.js
@@ -2,8 +2,13 @@ import React from "react";
 import "./RatingCircle.css";
 
 const RatingCircle = ({ value }) => {
+  const roundedValue = Math.round(value);
   return (
-    <div>{value && <div className="progress" data-value={value}></div>}</div>
+    <div>
+      {value && (
+        <div className="progress" data-value={roundedValue + "%"}></div>
+      )}
+    </div>
   );
 };
 
