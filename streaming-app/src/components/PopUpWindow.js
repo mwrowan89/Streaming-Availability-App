90193838aaeabf729adf951723ab34ffc25abad9
mwrowan89 <mwrowan89@gmail.com>
2024-07-01 12:20:56 -0600

    Added styling to popup window with more info

--- streaming-app/src/components/PopUpWindow.js
+++ streaming-app/src/components/PopUpWindow.js
@@ -18,9 +18,11 @@ const PopUpWindow = ({ isOpen, onRequestClose, result }) => {
             <div className="pop-up-results">
               <h1>{result.title}</h1>
               <p>{result.overview}</p>
-              <p>{result.release_date}</p>
-              <p>{result.vote_average}</p>
-              <p>{result.vote_count}</p>
+              <div className="pop-up-results-more-info">
+                <p>Release Date: {result.release_date}</p>
+                <p>TMDB Viewer Rating: {result.vote_average}</p>
+                <p>Vote Count: {result.vote_count}</p>
+              </div>
 
               <button onClick={onRequestClose}>Close</button>
             </div>
