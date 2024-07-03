cf32758cc2a291d33765670c7873957a5a9fd2fe
mwrowan89 <mwrowan89@gmail.com>
2024-07-03 13:12:40 -0600

    Fixed search bar crashing

--- streaming-app/src/pages/SearchPage.js
+++ streaming-app/src/pages/SearchPage.js
@@ -75,11 +75,11 @@ const SearchPage = () => {
         ) : (
           <h1 className="no-results">Sorry no results found</h1>
         )}
-        {searchResults.length === 0 && (
+        {/* {searchResults.length === 0 && (
           <h1 className="search-no-results">
             Sorry no results found for that title
           </h1>
-        )}
+        )} */}
       </div>
       <Footer />
     </div>
