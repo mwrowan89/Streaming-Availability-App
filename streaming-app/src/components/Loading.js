61d7b31ec46d5900d37def50dd07dabb8000de76
mwrowan89 <mwrowan89@gmail.com>
2024-07-03 14:17:15 -0600

    Added a loading component

---
+++ streaming-app/src/components/Loading.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div>
+      <div id="load">
+        <div>G</div>
+        <div>N</div>
+        <div>I</div>
+        <div>D</div>
+        <div>A</div>
+        <div>O</div>
+        <div>L</div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
